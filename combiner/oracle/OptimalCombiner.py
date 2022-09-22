7997a5c243a7ead84d68ef546df0db16d7394427
Dominik Kus <dominik@kdtk.de>
2022-09-22 14:36:51 +0200

    add option to exclude idss to optimal combiner

--- combiner/oracle/OptimalCombiner.py
+++ combiner/oracle/OptimalCombiner.py
@@ -8,12 +8,22 @@ class OptimalCombiner(Combiner):
 
     _name = "OptimalCombiner"
 
+    _optimalcombiner_default_settings = {
+        # Those IDSS are ignored
+        "exclude_idss": []
+    }
+
     def __init__(self, name=None):
         super().__init__(name=name)
+        self._add_default_settings(self._optimalcombiner_default_settings)
 
     def combine(self, msg):
         malicious = msg["malicious"] is not False
-        ids_outputs = list(msg["alerts"].values())
+        ids_outputs = [
+            v
+            for k, v in msg["alerts"].items()
+            if k not in self.settings["exclude_idss"]
+        ]
 
         if malicious in ids_outputs:
             # At least one is correct: give correct output
